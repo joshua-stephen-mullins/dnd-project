d581b51205ba5f6700b3e4a26390e414826d9bd9
Joshua Mullins <joshua.stephen.mullins@gmail.com>
2022-10-20 09:00:57 -0500

    completed ability mod function and added to sheet

--- js/char-sheet.js
+++ js/char-sheet.js
@@ -8,21 +8,24 @@ function d20RollFunction(){
 // variable for the d20 roll function
 let d20Roll = d20RollFunction();
 
-
-// WIP - function for calculating ability modifier when you input the ability score
+// function for calculating ability modifier when you input the ability score
 function modCalc (abilityScore){
-
+    return Math.floor(abilityScore - 10) / 2;
 }
 
 // WIP - function to check if proficient
-function isProficient(){
-    return
+function isProficient(charName, checkName) {
+    if (charName.proficiencies.indexOf('checkName') !== -1) {
+        return charName.profBonus
+    }
 }
 
 // function to do checks for primary stats
+
 function strCheck(charName) {
     return `Die roll ${d20Roll} | Strength Modifier ${charName.strMod} | Total: ${d20Roll + charName.strMod}`
 }
+
 function dexCheck(charName){
     return `Die roll ${d20Roll} | Dexterity Modifier ${charName.dexMod} | Total: ${d20Roll + charName.dexMod}`
 }
@@ -197,35 +200,37 @@ let cassidyAgustus = {};
     cassidyAgustus.proficiencies = ["Medicine", "Perception", "Religion", "Survival"];
     cassidyAgustus.profBonus = 2;
     cassidyAgustus.strScore = 10;
-    cassidyAgustus.strMod = 0;
+    cassidyAgustus.strMod = modCalc(cassidyAgustus.strScore);
     cassidyAgustus.dexScore = 12;
-    cassidyAgustus.dexMod = 1;
+    cassidyAgustus.dexMod =  modCalc(cassidyAgustus.dexScore);
     cassidyAgustus.conScore = 14;
-    cassidyAgustus.conMod = 2;
+    cassidyAgustus.conMod = modCalc(cassidyAgustus.conScore);
     cassidyAgustus.intScore = 14;
-    cassidyAgustus.intMod = 2;
+    cassidyAgustus.intMod = modCalc(cassidyAgustus.intScore);
     cassidyAgustus.wisScore = 17;
-    cassidyAgustus.wisMod = 3;
+    cassidyAgustus.wisMod = modCalc(cassidyAgustus.wisScore);
     cassidyAgustus.charScore = 8;
-    cassidyAgustus.charMod = -1;
+    cassidyAgustus.charMod = modCalc(cassidyAgustus.charScore);
 
-cassidyAgustus.acrobatics = cassidyAgustus.dexMod;
-cassidyAgustus.animalHandling = cassidyAgustus.wisMod;
-cassidyAgustus.arcana = cassidyAgustus.intMod;
-cassidyAgustus.athletics = cassidyAgustus.strMod;
-cassidyAgustus.deception = cassidyAgustus.charMod;
-cassidyAgustus.history = cassidyAgustus.intMod;
-cassidyAgustus.insight = cassidyAgustus.wisMod;
-cassidyAgustus.intimidation = cassidyAgustus.charMod;
-cassidyAgustus.investigation = cassidyAgustus.intMod;
-cassidyAgustus.nature = cassidyAgustus.intMod;
-cassidyAgustus.perception = cassidyAgustus.wisMod;
-cassidyAgustus.performance = cassidyAgustus.charMod;
-cassidyAgustus.persuasion = cassidyAgustus.charMod;
-cassidyAgustus.religion = cassidyAgustus.intMod;
-cassidyAgustus.slightOfHand = cassidyAgustus.dexMod;
-cassidyAgustus.stealth = cassidyAgustus.dexMod;
-cassidyAgustus.survival = cassidyAgustus.wisMod;
+    // ability checks
+
+    cassidyAgustus.acrobatics = cassidyAgustus.dexMod;
+    cassidyAgustus.animalHandling = cassidyAgustus.wisMod;
+    cassidyAgustus.arcana = cassidyAgustus.intMod;
+    cassidyAgustus.athletics = cassidyAgustus.strMod;
+    cassidyAgustus.deception = cassidyAgustus.charMod;
+    cassidyAgustus.history = cassidyAgustus.intMod;
+    cassidyAgustus.insight = cassidyAgustus.wisMod;
+    cassidyAgustus.intimidation = cassidyAgustus.charMod;
+    cassidyAgustus.investigation = cassidyAgustus.intMod;
+    cassidyAgustus.nature = cassidyAgustus.intMod;
+    cassidyAgustus.perception = cassidyAgustus.wisMod;
+    cassidyAgustus.performance = cassidyAgustus.charMod;
+    cassidyAgustus.persuasion = cassidyAgustus.charMod;
+    cassidyAgustus.religion = cassidyAgustus.intMod;
+    cassidyAgustus.slightOfHand = cassidyAgustus.dexMod;
+    cassidyAgustus.stealth = cassidyAgustus.dexMod;
+    cassidyAgustus.survival = cassidyAgustus.wisMod;
 
 
 
