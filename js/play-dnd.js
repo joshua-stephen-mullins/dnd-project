3487062454e1c9fba68d99142a043cfc5c023210
Joshua Mullins <joshua.stephen.mullins@gmail.com>
2022-11-13 12:36:42 -0600

    adjusted style of grasslands on homepage

--- js/play-dnd.js
+++ js/play-dnd.js
@@ -18,7 +18,14 @@ function activateGrasslandsStyle (){
     navbarElement.classList.remove('bg-light')
 
     let styleDropdown = document.getElementById('styleDropdown');
-    styleDropdown.style.backgroundColor = '#ab6e03';
+    // styleDropdown.style.backgroundColor = '#ab6e03';
+
+    let homepageBody = document.getElementById('homepageBody');
+    console.log(homepageBody);
+    homepageBody.style.backgroundColor = 'rgba(252,252,241,0.7)';
+    // homepageBody.style.opacity = '50%';
+    homepageBody.style.color = '#000000';
+    homepageBody.classList.add('pixel-box');
 }
 let grasslandsStyle = document.getElementById('styleGrassland');
 grasslandsStyle.addEventListener('click', activateGrasslandsStyle);
