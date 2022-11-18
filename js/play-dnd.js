caa1f67142e1445485e7b7f69ab99a78ebed559f
Joshua Mullins <joshua.stephen.mullins@gmail.com>
2022-11-18 16:57:50 -0600

    added class functionality to character creation

--- js/play-dnd.js
+++ js/play-dnd.js
@@ -1,80 +1,93 @@
 "use strict";
 
 
-$(document).ready(function(){
-
+$(document).ready(function () {
 
 
 //    character creation
-let newChar = {};
+    let newChar = {};
     newChar.race = '';
     newChar.class = '';
     newChar.background = '';
     newChar.equipment = [];
 
-$('.raceButton').click(function(){
-    $.get('https://www.dnd5eapi.co/api/races/' + this.value).done(function(data){
-        $('.racialBonus').each(function(){
-            $('span').html('0');
+    $('.raceButton').click(function () {
+        $.get('https://www.dnd5eapi.co/api/races/' + this.value).done(function (data) {
+            $('.racialBonus').each(function () {
+                $('span').html('0');
+            });
+            newChar.race = data;
+            $(newChar.race.ability_bonuses).each(function () {
+                console.log(this.ability_score.index);
+                $('#racialBonus_' + this.ability_score.index).html(this.bonus);
+            })
         });
-        newChar.race = data;
-        $(newChar.race.ability_bonuses).each(function() {
-            console.log(this.ability_score.index);
-            $('#racialBonus_' + this.ability_score.index).html(this.bonus);
-        })
-    });
-})
-
+    })
 //takes you to class tab when you click a race
-    $(".raceButton").click(function() {
+    $(".raceButton").click(function () {
         $('#class-tab').tab('show');
+    })
+
+    $('.classButton').click(function () {
+        $.get('https://www.dnd5eapi.co/api/classes/' + this.value).done(function(data) {
+            newChar.class = data;
+            console.log(newChar);
+        })
+    })
+
+    $(".classButton").click(function () {
+        $('#statistics-tab').tab('show');
     })
 
 
 //change style functions
 //grasslands
-function activateGrasslandsStyle (){
-    let bodyElement = document.getElementsByTagName('body')[0];
-    bodyElement.style.backgroundImage = 'url("../img/pixel-sky.png")';
-    bodyElement.style.backgroundSize = 'cover';
-    bodyElement.style.backgroundRepeat = 'no-repeat';
-    bodyElement.style.backgroundAttachment = 'fixed';
+        function activateGrasslandsStyle() {
+            let bodyElement = document.getElementsByTagName('body')[0];
+            bodyElement.style.backgroundImage = 'url("../img/pixel-sky.png")';
+            bodyElement.style.backgroundSize = 'cover';
+            bodyElement.style.backgroundRepeat = 'no-repeat';
+            bodyElement.style.backgroundAttachment = 'fixed';
 
-    let navbarElement = document.getElementsByTagName('nav')[0];
-    console.log(navbarElement);
-    navbarElement.style.backgroundColor = '#efb73f';
-    navbarElement.classList.remove('bg-light')
+            let navbarElement = document.getElementsByTagName('nav')[0];
+            console.log(navbarElement);
+            navbarElement.style.backgroundColor = '#efb73f';
+            navbarElement.classList.remove('bg-light')
 
-    let styleDropdown = document.getElementById('styleDropdown');
-    // styleDropdown.style.backgroundColor = '#ab6e03';
-}
-let grasslandsStyle = document.getElementById('styleGrassland');
-grasslandsStyle.addEventListener('click', activateGrasslandsStyle);
+            let styleDropdown = document.getElementById('styleDropdown');
+            // styleDropdown.style.backgroundColor = '#ab6e03';
+        }
 
-function activateGrasslandsStyleHomePage (){
-    let homepageBody = document.getElementById('homepageBody');
-    homepageBody.style.backgroundColor = 'rgba(252,252,241,0.7)';
-    homepageBody.style.color = '#000000';
-    homepageBody.classList.add('pixel-box');
-}
-grasslandsStyle.addEventListener('click', activateGrasslandsStyleHomePage);
+        let grasslandsStyle = document.getElementById('styleGrassland');
+        grasslandsStyle.addEventListener('click', activateGrasslandsStyle);
 
-function activateGrasslandsStyleRacePage (){
-    let raceCarousel = document.getElementById('raceCarousel');
-    raceCarousel.style.backgroundColor = 'rgba(252,252,241,0.7)';
-    raceCarousel.style.color = '#000000';
-    raceCarousel.classList.add('pixel-box');
-}
-grasslandsStyle.addEventListener('click', activateGrasslandsStyleRacePage);
+        function activateGrasslandsStyleHomePage() {
+            let homepageBody = document.getElementById('homepageBody');
+            homepageBody.style.backgroundColor = 'rgba(252,252,241,0.7)';
+            homepageBody.style.color = '#000000';
+            homepageBody.classList.add('pixel-box');
+        }
 
-function activateGrasslandsStyleClassPage (){
-    let classCards = document.getElementsByClassName('classCard');
+        grasslandsStyle.addEventListener('click', activateGrasslandsStyleHomePage);
 
-    for (let i = 0; i < classCards.length; i++){
-        classCards[i].style.backgroundColor = 'rgba(252,252,241,0.7)';
-        classCards[i].style.color = '#000000';
-    }
-}
-grasslandsStyle.addEventListener('click', activateGrasslandsStyleClassPage);
+        function activateGrasslandsStyleRacePage() {
+            let raceCarousel = document.getElementById('raceCarousel');
+            raceCarousel.style.backgroundColor = 'rgba(252,252,241,0.7)';
+            raceCarousel.style.color = '#000000';
+            raceCarousel.classList.add('pixel-box');
+        }
 
-});
+        grasslandsStyle.addEventListener('click', activateGrasslandsStyleRacePage);
+
+        function activateGrasslandsStyleClassPage() {
+            let classCards = document.getElementsByClassName('classCard');
+
+            for (let i = 0; i < classCards.length; i++) {
+                classCards[i].style.backgroundColor = 'rgba(252,252,241,0.7)';
+                classCards[i].style.color = '#000000';
+            }
+        }
+
+        grasslandsStyle.addEventListener('click', activateGrasslandsStyleClassPage);
+
+    });
