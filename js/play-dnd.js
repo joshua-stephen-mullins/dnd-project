78e9ef93cf9984f708829c0cb2b7822b7c2991ce
Joshua Mullins <joshua.stephen.mullins@gmail.com>
2022-12-02 15:52:44 -0600

    reworking functions for efficiency

--- js/play-dnd.js
+++ js/play-dnd.js
@@ -16,7 +16,7 @@ $(document).ready(function () {
     $('.raceButton').click(function (e) {
         e.preventDefault();
         //retrieves race info based on race button clicked
-        $.get('https://www.dnd5eapi.co/api/races/' + this.value).done(function (data) {
+        $.get(`https://www.dnd5eapi.co/api/races/${this.value}`).done(function (data) {
             //resets racial bonuses to 0 on statistics page - basically just here in case you go back and click a different race
             $('.racialBonus').each(function () {
                 $('span').html('0');
@@ -27,25 +27,25 @@ $(document).ready(function () {
             newChar.race = data;
             //populates statistics page with racial bonuses
             $(newChar.race.ability_bonuses).each(function () {
-                $('#racialBonus_' + this.ability_score.index).html(this.bonus);
+                $(`#racialBonus_${this.ability_score.index}`).html(this.bonus);
             })
             //populating various fields on the features tab with race info from api
-            $('#alignment').html('<span class="fw-bold">Alignment: </span>' + newChar.race.alignment);
+            $('#alignment').html(`<span class="fw-bold">Alignment: </span>${newChar.race.alignment}`);
             $('#age').html('<span class="fw-bold">Age: </span>' + newChar.race.age);
-            $('#size-desc').html('<span class="fw-bold">Size: </span>' + newChar.race.size_description);
+            $('#size-desc').html(`<span class="fw-bold">Size: </span>${newChar.race.size_description}`);
+
             //function to provide dropdown if race has additional language choice options
             function languageFeaturesPopulation(e) {
                 if (newChar.race.hasOwnProperty('language_options')) {
-                    console.log('has lang thing')
-                    $('#languages').html('<span class="fw-bold">Languages: </span>' + newChar.race.language_desc + '<br><select class="text-center" id="language_selection"></select>');
+                    $('#languages').html(`<span class="fw-bold">Languages: </span>${newChar.race.language_desc}<br><select class="text-center" id="language_selection"></select>`);
                     newChar.race.language_options.from.options.forEach(function (option) {
-                        $('#language_selection').append('<option value="' + option.item.index + '">' + option.item.name + '</option>')
+                        $('#language_selection').append(`<option value="${option.item.index}">${option.item.name}</option>`)
                     })
                 } else {
-                    console.log('no lang thing')
-                    $('#languages').html('<span class="fw-bold">Languages: </span>' + newChar.race.language_desc)
+                    $('#languages').html(`<span class="fw-bold">Languages: </span>${newChar.race.language_desc}`)
                 }
             }
+
             languageFeaturesPopulation();
             //populates traits section of racial features
             if (newChar.race.traits.length === 0) {
@@ -54,26 +54,21 @@ $(document).ready(function () {
                 $('#traitCards').html('');
                 $('#traitsHeader').html('<h3>Traits</h3>');
                 newChar.race.traits.forEach(function (trait) {
-                    $.get('https://www.dnd5eapi.co/api/traits/' + trait.index).done(function (data) {
+                    $.get(`https://www.dnd5eapi.co/api/traits/${trait.index}`).done(function (data) {
                         let trait = data;
-                        $('#traitCards').append('<div class="p-0" id="' + trait.index + '">' +
-                            '<p class="text-decoration-underline mb-0 text-start">' + trait.name + '</p>' +
-                            '<p>' + trait.desc + '</p>' +
-                            '</div>');
+                        $('#traitCards').append(`<div class="p-0" id="${trait.index}"><p class="text-decoration-underline mb-0 text-start">${trait.name}</p><p>${trait.desc}</p></div>`);
                         //conditional to create select option for traits that has a choice
                         if (trait.hasOwnProperty('proficiency_choices')) {
-                            for (let i = 1; i <= trait.proficiency_choices.choose; i++){
-                            $('#' + trait.index).append('<select class="text-center" id="' + trait.index + '_selection' + i + '"></select>');
-                            trait.proficiency_choices.from.options.forEach((function (choice) {
-                                $('#' + trait.index + '_selection' + i).append('<option value="' + choice.item.index + '">' +
-                                    choice.item.name +
-                                    '</option>')
-                            }))}
+                            for (let i = 1; i <= trait.proficiency_choices.choose; i++) {
+                                $(`#${trait.index}`).append(`<select class="text-center" id="${trait.index}_selection${i}"></select>`);
+                                trait.proficiency_choices.from.options.forEach((function (choice) {
+                                    $('#' + trait.index + '_selection' + i).append(`<option value="${choice.item.index}">${choice.item.name}</option>`)
+                                }))
+                            }
                         }
                         if (trait.hasOwnProperty('proficiencies')) {
-                            trait.proficiencies.forEach(function(proficiency){
+                            trait.proficiencies.forEach(function (proficiency) {
                                 newChar.proficiencies.race.push(proficiency);
-                                console.log(newChar);
                             })
                         }
                         //conditional to populate table for dragonborn choices
@@ -93,9 +88,7 @@ $(document).ready(function () {
                                 '<select id="draconic-ancestry-selection"' +
                                 '</select>')
                             trait.trait_specific.subtrait_options.from.options.forEach((function (choice) {
-                                $('#draconic-ancestry-selection').append('<option value="' + choice.item.index + '">' +
-                                    choice.item.name +
-                                    '</option>')
+                                $('#draconic-ancestry-selection').append(`<option value="${choice.item.index}">${choice.item.name}</option>`)
                             }))
                         }
                     })
@@ -106,28 +99,25 @@ $(document).ready(function () {
     //subrace selection
     $('.subRaceButton').click(function () {
         //pulls subrace data from api using selection value from dropdown
-        $.get('https://www.dnd5eapi.co/api/subraces/' + $('#subRace_' + this.value).find(':selected').val()).done(function (data) {
+        $.get('https://www.dnd5eapi.co/api/subraces/' + $(`#subRace_${this.value}`).find(':selected').val()).done(function (data) {
             newChar.subRace = data;
-            //added additonal statistical bonuses from subclass to statistics tab
+            //added additional statistical bonuses from subclass to statistics tab
             $(newChar.subRace.ability_bonuses).each(function () {
-                $('#racialBonus_' + this.ability_score.index).html(this.bonus);
+                $(`#racialBonus_${this.ability_score.index}`).html(this.bonus);
             })
             //populates racial traits from subrace to racial features section on features tab
             newChar.subRace.racial_traits.forEach(function (trait) {
-                $.get('https://www.dnd5eapi.co/api/traits/' + trait.index).done(function (data) {
+                $.get(`https://www.dnd5eapi.co/api/traits/${trait.index}`).done(function (data) {
                     let trait = data;
-                    $('#traitCards').append('<div class="p-0" id="' + trait.index + '">' +
-                        '<p class="text-decoration-underline m-0">' + trait.name + '</p>' +
-                        '<p>' + trait.desc + '</p>' +
-                        '</div>');
+                    $('#traitCards').append(`<div class="p-0" id="${trait.index}"><p class="text-decoration-underline m-0">${trait.name}</p><p>${trait.desc}</p></div>`);
                     if (trait.hasOwnProperty('language_options')) {
-                        $('#' + trait.index).append('<select id="' + trait.index + '_selection"></select>');
+                        $(`#${trait.index}`).append('<select id="' + trait.index + '_selection"></select>');
                         trait.language_options.from.options.forEach(function (option) {
                             $('#' + trait.index + '_selection').append('<option value="' + option.item.index + '">' + option.item.name + '</option>')
                         })
                     }
                     if (trait.hasOwnProperty('proficiencies')) {
-                        trait.proficiencies.forEach(function(proficiency){
+                        trait.proficiencies.forEach(function (proficiency) {
                             newChar.proficiencies.race.push(proficiency);
                             console.log(newChar);
                         })
@@ -163,62 +153,39 @@ $(document).ready(function () {
     });
     //assigns a class when a class card is clicked and takes you to statistics tab when you click a class
     $('.classButton').click(function (e) {
+        e.preventDefault();
         $.get('https://www.dnd5eapi.co/api/classes/' + this.value).done(function (data) {
-            e.preventDefault();
             newChar.class = data;
             $('#statistics-tab').tab('show');
         })
     })
     //function to check statistics dropdowns and disable already chosen options
-    let abilityScoreCalculate = $('.abilityScoreSelection').change(function () {
+    $('.abilityScoreSelection').change(function () {
         let abilityScoreArray = [];
         $('.abilityScoreSelection').each(function () {
             abilityScoreArray.push(this.value)
         })
-        if (abilityScoreArray.indexOf('abilityScore-8') !== -1) {
-            $('.value-8').prop('disabled', true);
-        } else {
-            $('.value-8').prop('disabled', false);
+        for (let i = 8; i <= 15; i++) {
+            if (abilityScoreArray.indexOf('abilityScore-' + i) !== -1) {
+                $('.value-' + i).prop('disabled', true);
+            } else {
+                $('.value-' + i).prop('disabled', false);
+            }
+            //assigns dropdown values into stats card
+            $('#baseScore_str').html(parseInt(abilityScoreArray[0].substring(13)));
+            $('#baseScore_dex').html(parseInt(abilityScoreArray[1].substring(13)));
+            $('#baseScore_con').html(parseInt(abilityScoreArray[2].substring(13)));
+            $('#baseScore_int').html(parseInt(abilityScoreArray[3].substring(13)));
+            $('#baseScore_wis').html(parseInt(abilityScoreArray[4].substring(13)));
+            $('#baseScore_cha').html(parseInt(abilityScoreArray[5].substring(13)));
+            //adds chosen numbers from dropdowns with racial modifier and populates total
+            $('#abilityScoreTotal_str').html(parseInt($('#baseScore_str')[0].innerText) + parseInt($('#racialBonus_str')[0].innerText))
+            $('#abilityScoreTotal_dex').html(parseInt($('#baseScore_dex')[0].innerText) + parseInt($('#racialBonus_dex')[0].innerText))
+            $('#abilityScoreTotal_con').html(parseInt($('#baseScore_con')[0].innerText) + parseInt($('#racialBonus_con')[0].innerText))
+            $('#abilityScoreTotal_int').html(parseInt($('#baseScore_int')[0].innerText) + parseInt($('#racialBonus_int')[0].innerText))
+            $('#abilityScoreTotal_wis').html(parseInt($('#baseScore_wis')[0].innerText) + parseInt($('#racialBonus_wis')[0].innerText))
+            $('#abilityScoreTotal_cha').html(parseInt($('#baseScore_cha')[0].innerText) + parseInt($('#racialBonus_cha')[0].innerText))
         }
-        if (abilityScoreArray.indexOf('abilityScore-10') !== -1) {
-            $('.value-10').prop('disabled', true);
-        } else {
-            $('.value-10').prop('disabled', false);
-        }
-        if (abilityScoreArray.indexOf('abilityScore-12') !== -1) {
-            $('.value-12').prop('disabled', true);
-        } else {
-            $('.value-12').prop('disabled', false);
-        }
-        if (abilityScoreArray.indexOf('abilityScore-13') !== -1) {
-            $('.value-13').prop('disabled', true);
-        } else {
-            $('.value-13').prop('disabled', false);
-        }
-        if (abilityScoreArray.indexOf('abilityScore-14') !== -1) {
-            $('.value-14').prop('disabled', true);
-        } else {
-            $('.value-14').prop('disabled', false);
-        }
-        if (abilityScoreArray.indexOf('abilityScore-15') !== -1) {
-            $('.value-15').prop('disabled', true);
-        } else {
-            $('.value-15').prop('disabled', false);
-        }
-        //assigns dropdown values into stats card
-        $('#baseScore_str').html(parseInt(abilityScoreArray[0].substring(13)));
-        $('#baseScore_dex').html(parseInt(abilityScoreArray[1].substring(13)));
-        $('#baseScore_con').html(parseInt(abilityScoreArray[2].substring(13)));
-        $('#baseScore_int').html(parseInt(abilityScoreArray[3].substring(13)));
-        $('#baseScore_wis').html(parseInt(abilityScoreArray[4].substring(13)));
-        $('#baseScore_cha').html(parseInt(abilityScoreArray[5].substring(13)));
-        //adds chosen numbers from dropdowns with racial modifier and populates total
-        $('#abilityScoreTotal_str').html(parseInt($('#baseScore_str')[0].innerText) + parseInt($('#racialBonus_str')[0].innerText))
-        $('#abilityScoreTotal_dex').html(parseInt($('#baseScore_dex')[0].innerText) + parseInt($('#racialBonus_dex')[0].innerText))
-        $('#abilityScoreTotal_con').html(parseInt($('#baseScore_con')[0].innerText) + parseInt($('#racialBonus_con')[0].innerText))
-        $('#abilityScoreTotal_int').html(parseInt($('#baseScore_int')[0].innerText) + parseInt($('#racialBonus_int')[0].innerText))
-        $('#abilityScoreTotal_wis').html(parseInt($('#baseScore_wis')[0].innerText) + parseInt($('#racialBonus_wis')[0].innerText))
-        $('#abilityScoreTotal_cha').html(parseInt($('#baseScore_cha')[0].innerText) + parseInt($('#racialBonus_cha')[0].innerText))
     })
     //backgrounds tab functions
     $.get('https://www.dnd5eapi.co/api/backgrounds/').done(function (data) {
@@ -246,7 +213,7 @@ $(document).ready(function () {
                     //     console.log('has warhammers');
                     // }
                     if (background.hasOwnProperty('starting_proficiencies')) {
-                        background.starting_proficiencies.forEach(function(proficiency){
+                        background.starting_proficiencies.forEach(function (proficiency) {
                             newChar.proficiencies.background.push(proficiency);
                             console.log(newChar);
                         })
