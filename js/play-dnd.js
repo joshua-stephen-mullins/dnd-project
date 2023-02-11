2e1b5191ef0cab36f611d7bb8dbbbd957fa74c58
Joshua Mullins <joshua.stephen.mullins@gmail.com>
2023-02-10 23:32:42 -0600

    finished style removal

--- js/play-dnd.js
+++ js/play-dnd.js
@@ -348,33 +348,4 @@ $(document).ready(function () {
         })
     })
 
-//change style functions
-//grasslands
-    function activateGrasslandsStyle() {
-        let bodyElement = document.getElementsByTagName('body')[0];
-        bodyElement.style.backgroundImage = 'url("../img/pixel-sky.png")';
-        bodyElement.style.backgroundSize = 'cover';
-        bodyElement.style.backgroundRepeat = 'no-repeat';
-        bodyElement.style.backgroundAttachment = 'fixed';
-
-        let navbarElement = document.getElementsByTagName('nav')[0];
-        console.log(navbarElement);
-        navbarElement.style.backgroundColor = '#efb73f';
-        navbarElement.classList.remove('bg-light')
-
-        let styleDropdown = document.getElementById('styleDropdown');
-        // styleDropdown.style.backgroundColor = '#ab6e03';
-    }
-
-    let grasslandsStyle = document.getElementById('styleGrassland');
-    grasslandsStyle.addEventListener('click', activateGrasslandsStyle);
-
-    function activateGrasslandsStyleHomePage() {
-        let homepageBody = document.getElementById('homepageBody');
-        homepageBody.style.backgroundColor = 'rgba(252,252,241,0.7)';
-        homepageBody.style.color = '#000000';
-        homepageBody.classList.add('pixel-box');
-    }
-
-
 })
